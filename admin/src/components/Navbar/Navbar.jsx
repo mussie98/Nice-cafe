c3a14ea665fd828e2b0781071794fbe80e80fa82
mussie98 <147577933+mussie98@users.noreply.github.com>
2025-10-24 13:26:40 -0400

    commenting out

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img className='logo' src={assets.logo} alt='logo' />
-      <img className='profile' src={assets.profile_image} alt='profile' />
+      {/* <img className='profile' src={assets.profile_image} alt='profile' /> */}
     </div>
   );
 };
